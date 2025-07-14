bb2cea30a58835fa952c63ca6e088bc83a78b9f6
Line Sow <linetheboss359@gmail.com>
2025-07-14 22:02:08 +0000

    added speed insight

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-gi# Firebase Studio
+ gi# Firebase Studio
 
 This is a NextJS starter in Firebase Studio.
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,8 @@ import type { Metadata } from 'next';
 import { Toaster } from "@/components/ui/toaster";
 import { AuthProvider } from "@/hooks/use-auth";
 import './globals.css';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: 'SenAgriConnect',
@@ -26,6 +27,7 @@ export default function RootLayout({
         <AuthProvider>
           {children}
           <Analytics />
+          <SpeedInsights />
           <Toaster />
         </AuthProvider>
       </body>
